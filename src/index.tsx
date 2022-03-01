5887ab09f16075b5eb74c99b87ada79a1dbadf99
Maxime Cazade <maxime.cazade@tilkal.com>
2022-03-02 00:13:56 +0100

    refacto (main): refacto page links

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Trips from './components/trips'
+import Trips from './pages/trips'
 import Header from './components/header'
 import './index.css'
-import Home from './pages/Home'
+import Home from './pages/home/Home'
 
 ReactDOM.render(
   <React.StrictMode>
