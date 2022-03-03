8a10243a6310dd6c124182833d84822b38987621
Maxime Cazade <maxime.cazade@tilkal.com>
2022-03-03 21:37:29 +0100

    delete home & header

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Trips from './pages/trips'
-import Header from './components/header'
 import './index.css'
-import Home from './pages/home/Home'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="trips" element={<Trips />} />
-      </Routes>
-    </BrowserRouter>
+    <Trips />
   </React.StrictMode>,
   document.getElementById('root')
 )
