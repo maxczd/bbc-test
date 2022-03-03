75e4ca5ecb5131e8cd10e49dbf5e532cac80437a
Maxime Cazade <maxime.cazade@tilkal.com>
2022-03-03 21:12:35 +0100

    change tests files ext

--- src/pages/trips/index.test.tsx
+++
@@ -1,8 +0,0 @@
-import Trips from '.'
-import { render } from '@testing-library/react'
-
-describe('Trips', () => {
-  test('Should render without crash', async () => {
-    render(<Trips />)
-  })
-})
